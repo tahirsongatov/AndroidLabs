3f3d8160774bf67ede1d213d90c3196b3cb7c711
tahirsongatov <songatov@gmail.com>
2024-09-08 21:14:30 +0300

    Surround the text with a Container

--- GreetingCard/app/src/main/java/com/example/greetingcard/MainActivity.kt
+++ GreetingCard/app/src/main/java/com/example/greetingcard/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -32,10 +33,12 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hi, my name is $name!",
-        modifier = modifier
-    )
+    Box {
+        Text(
+            text = "Hi, my name is $name!",
+            modifier = modifier
+        )
+    }
 }
 
 @Preview(showBackground = true)
