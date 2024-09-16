d91066d181d81506f3b72cb9cd676a80afbc29d1
tahirsongatov <songatov@gmail.com>
2024-09-16 20:41:18 +0300

    Add padding and align to from text

--- HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -49,7 +50,9 @@ fun GreetingText(message: String, from: String, modifier: Modifier = Modifier){
         )
         Text(
             text = from,
-            fontSize = 36.sp
+            fontSize = 36.sp,
+            modifier = modifier.padding(16.dp)
+                .align(alignment = Alignment.End)
         )
     }
 }
