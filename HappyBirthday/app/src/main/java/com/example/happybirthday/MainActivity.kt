9dd1ed44b4c48e92612c70aa6bcd6d4ac021d0da
tahirsongatov <songatov@gmail.com>
2024-09-16 22:13:03 +0300

    Move greeting text to string resource

--- HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -17,6 +17,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -32,7 +33,7 @@ class MainActivity : ComponentActivity() {
                 color = MaterialTheme.colorScheme.background
             ) {
                 GreetingImage(
-                    message = "Happy Birthday Sam!",
+                    message = stringResource(R.string.happy_birthday_text),
                     from = "From Tagir"
                 )
             }
@@ -75,7 +76,9 @@ fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier){
         GreetingText(
             message = message,
             from = from,
-            modifier = Modifier.fillMaxSize().padding(8.dp)
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp)
         )
     }
 }
