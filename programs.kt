f03645b964b69bb140be08793bc5c44a166377c8
tahirsongatov <songatov@gmail.com>
2024-09-28 16:17:18 +0300

    When statements

--- programs.kt
+++ programs.kt
@@ -199,3 +199,72 @@ fun main() {
         println("Invalid traffic-light color")
     }
 }
+// Traffic light with when statement
+fun main() {
+    val trafficLightColor = "Black"
+
+    when (trafficLightColor) {
+        "Red" -> println("Stop")
+        "Yellow" -> println("Slow")
+        "Green" -> println("Go")
+        else -> println("Invalid traffic-light color")
+    }
+}
+// More complex condition in a when
+fun main() {
+    val x = 3
+    when (x) {
+        2, 3, 5, 7 -> println("x is a prime number between 1 and 10.")
+        else -> println("x isn't a prime number between 1 and 10.")
+    }
+}
+fun main() {
+    val x = 3
+    
+    when (x) {
+        2, 3, 5, 7 -> println("x is a prime number between 1 and 10.")
+        in 1..10 -> println("x is a number between 1 and 10, but not a prime number.")
+        else -> println("x isn't a prime number between 1 and 10.")
+    }
+}
+fun main() {
+    val x: Any = 4
+
+    when (x) {
+        2, 3, 5, 7 -> println("x is a prime number between 1 and 10.")
+        in 1..10 -> println("x is a number between 1 and 10, but not a prime number.")
+        is Int -> println("x is an integer number, but not between 1 and 10.")
+        else -> println("x isn't an integer number.")
+    }
+}
+
+// Traffic light
+fun main() {
+    val trafficLightColor = "Amber"
+
+    when (trafficLightColor) {
+        "Red" -> println("Stop")
+        "Yellow", "Amber" -> println("Slow")
+        "Green" -> println("Go")
+        else -> println("Invalid traffic-light color")
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
