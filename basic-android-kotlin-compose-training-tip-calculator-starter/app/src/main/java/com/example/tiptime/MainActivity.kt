1e9c0a8febd8c5e0cf80c2c9eda6990e319527c7
Tagir Songatov <songatov@gmail.com>
2024-11-09 12:37:36 +0300

    Use state in Compose

--- basic-android-kotlin-compose-training-tip-calculator-starter/app/src/main/java/com/example/tiptime/MainActivity.kt
+++ basic-android-kotlin-compose-training-tip-calculator-starter/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -83,8 +83,9 @@ fun TipTimeLayout() {
 }
 @Composable
 fun EditNumberField(modifier: Modifier = Modifier) {
+    val amountInput = "0"
     TextField(
-        value = "",
+        value = amountInput,
         onValueChange = {},
         modifier = modifier
     )
