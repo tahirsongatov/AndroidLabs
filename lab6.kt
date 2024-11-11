d811d470f134f261e6ff824b3ee7d9bac6da0403
Tagir Songatov <songatov@gmail.com>
2024-11-11 21:02:49 +0300

    Define a data class

--- lab6.kt
+++ lab6.kt
@@ -22,4 +22,19 @@ fun main() {
     val question2 = Question<Boolean>("The sky is green. True or false", false, Difficulty.EASY)
     val question3 = Question<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
 }
+//data class
+data class Question<T>(
+    val questionText: String,
+    val answer: T,
+    val difficulty: Difficulty,
+)
+enum class Difficulty {
+    EASY, MEDIUM, HARD
+}
 
+fun main() {
+    val question1 = Question<String>("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
+    val question2 = Question<Boolean>("The sky is green. True or false", false, Difficulty.EASY)
+    val question3 = Question<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
+    println(question1.toString())
+}
