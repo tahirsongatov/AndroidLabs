08539f9159c50349a657e6906d5cf5ee1a065a07
Tagir Songatov <songatov@gmail.com>
2024-11-11 23:57:41 +0300

    Practice task 3

--- lab6Practic.kt
+++ lab6Practic.kt
@@ -36,3 +36,26 @@ fun main(){
     )
     println(event)
 }
+// Task 3
+data class Event (
+    val title: String,
+    val description: String? = null,
+    val daypart: Daypart,
+    val durationInMinutes: Int,
+)
+enum class Daypart {
+    Morning, Afternoon,Evening
+}
+fun main(){
+    val events = mutableListOf(
+    	Event(title = "Wake up", description = "Time to get up", daypart = Daypart.Morning, durationInMinutes = 0),
+       	Event(title = "Eat breakfast", daypart = Daypart.Morning, durationInMinutes = 15),
+        Event(title = "Learn about Kotlin", daypart = Daypart.Afternoon, durationInMinutes = 30),
+        Event(title = "Practice Compose", daypart = Daypart.Afternoon, durationInMinutes = 60),
+        Event(title = "Watch latest DevBytes video", daypart = Daypart.Afternoon, durationInMinutes = 10),
+        Event(title = "Check out latest Android Jetpack library", daypart = Daypart.Evening, durationInMinutes = 45),
+    )
+    events.forEach{
+        println(it)
+    }
+}
