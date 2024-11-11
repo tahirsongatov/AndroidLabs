e17a3cd6eeeadc493a9c9fa95440229a51b46772
Tagir Songatov <songatov@gmail.com>
2024-11-11 22:43:13 +0300

    Lists

--- lab6.kt
+++ lab6.kt
@@ -231,3 +231,23 @@ fun main(){
 	val newSolarSystem = arrayOf("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Pluto")
 	println(newSolarSystem[8])
 }
+// lists
+fun main() {
+    val solarSystem = mutableListOf("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune")
+    solarSystem.add("Pluto")
+    solarSystem.add(3, "Theia")
+    solarSystem[3] = "Future Moon"
+    
+    for (planet in solarSystem) {
+        println(planet)
+    }
+    solarSystem.removeAt(9)
+	solarSystem.remove("Future Moon")
+    println()
+    println("Future Moon" in solarSystem)
+    
+	for (planet in solarSystem) {
+        println(planet)
+    }
+}
+
