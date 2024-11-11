95a693c1b301214474535cb063738a98a28c45cb
Tagir Songatov <songatov@gmail.com>
2024-11-11 22:55:00 +0300

    Sets

--- lab6.kt
+++ lab6.kt
@@ -250,4 +250,18 @@ fun main() {
         println(planet)
     }
 }
+// sets
+fun main() {
+    val solarSystem = mutableSetOf("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune")
+	println(solarSystem.size)
+	solarSystem.add("Pluto")
+	println(solarSystem.size)
+	println(solarSystem.contains("Pluto"))
+    solarSystem.add("Pluto")
+	println(solarSystem.size)
+	solarSystem.remove("Pluto")
+    println(solarSystem.size)
+    println(solarSystem.contains("Pluto"))
+
+}
 
