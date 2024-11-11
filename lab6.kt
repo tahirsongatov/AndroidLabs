d07248ddae31d1697d2862d5fad4b735c0acd9c8
Tagir Songatov <songatov@gmail.com>
2024-11-11 21:44:25 +0300

    Interfaces

--- lab6.kt
+++ lab6.kt
@@ -106,5 +106,45 @@ fun main() {
     Quiz.printProgressBar()
     println(question1.toString())
 }
+// interface
+data class Question<T>(
+    val questionText: String,
+    val answer: T,
+    val difficulty: Difficulty,
+)
+enum class Difficulty {
+    EASY, MEDIUM, HARD
+}
+interface ProgressPrintable {
+    val progressText: String
+    fun printProgressBar()
+}
+
+class Quiz: ProgressPrintable {
+    val question1 = Question<String>("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
+    val question2 = Question<Boolean>("The sky is green. True or false", false, Difficulty.EASY)
+    val question3 = Question<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
+	override val progressText: String 
+    	get() = "${answered} of ${total} answered"
+
+    companion object StudentProgress {
+        var total: Int = 10
+        var answered: Int = 3
+    }
+    override fun printProgressBar() {
+        repeat(Quiz.answered) { print("▓") }
+        repeat(Quiz.total - Quiz.answered) { print("▒") }
+        println(progressText)
+	}
+}
+
+fun main() {
+    val question1 = Question<String>("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
+    val question2 = Question<Boolean>("The sky is green. True or false", false, Difficulty.EASY)
+    val question3 = Question<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
+    println("${Quiz.answered} of ${Quiz.total} answered.")
+    Quiz().printProgressBar()
+    println(question1.toString())
+}
 
 
