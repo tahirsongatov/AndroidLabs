b2fc4c70412a6d8ca641a4afd46abb5c9caae73c
Tagir Songatov <songatov@gmail.com>
2024-11-11 21:18:04 +0300

    Companion object and sngleton

--- lab6.kt
+++ lab6.kt
@@ -38,3 +38,35 @@ fun main() {
     val question3 = Question<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
     println(question1.toString())
 }
+// companion object and singleton
+object StudentProgress {
+        var total: Int = 10
+        var answered: Int = 3
+}
+data class Question<T>(
+    val questionText: String,
+    val answer: T,
+    val difficulty: Difficulty,
+)
+enum class Difficulty {
+    EASY, MEDIUM, HARD
+}
+class Quiz {
+    val question1 = Question<String>("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
+    val question2 = Question<Boolean>("The sky is green. True or false", false, Difficulty.EASY)
+    val question3 = Question<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
+
+    companion object StudentProgress {
+        var total: Int = 10
+        var answered: Int = 3
+    }
+}
+
+fun main() {
+    val question1 = Question<String>("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
+    val question2 = Question<Boolean>("The sky is green. True or false", false, Difficulty.EASY)
+    val question3 = Question<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
+    println("${Quiz.answered} of ${Quiz.total} answered.")
+    println(question1.toString())
+}
+
