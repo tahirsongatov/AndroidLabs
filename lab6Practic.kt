217520f15445d0348938c84f9025cecb809385e8
Tagir Songatov <songatov@gmail.com>
2024-11-12 00:08:33 +0300

    Practice task 5

--- lab6Practic.kt
+++ lab6Practic.kt
@@ -83,3 +83,29 @@ fun main(){
     }
     println("You have ${shorthandEvents.size} short events.")
 }
+// Task 5
+data class Event (
+    val title: String,
+    val description: String? = null,
+    val daypart: Daypart,
+    val durationInMinutes: Int,
+)
+enum class Daypart {
+    Morning, Afternoon,Evening
+}
+fun main(){
+    val events = mutableListOf(
+    	Event(title = "Wake up", description = "Time to get up", daypart = Daypart.Morning, durationInMinutes = 0),
+       	Event(title = "Eat breakfast", daypart = Daypart.Morning, durationInMinutes = 15),
+        Event(title = "Learn about Kotlin", daypart = Daypart.Afternoon, durationInMinutes = 30),
+        Event(title = "Practice Compose", daypart = Daypart.Afternoon, durationInMinutes = 60),
+        Event(title = "Watch latest DevBytes video", daypart = Daypart.Afternoon, durationInMinutes = 10),
+        Event(title = "Check out latest Android Jetpack library", daypart = Daypart.Evening, durationInMinutes = 45),
+    )
+    val daypartEvents = events.groupBy {
+        it.daypart
+    }
+    daypartEvents.forEach { (daypart,events)->
+        println("${itdaypart}: ${events.size} events.")
+    }
+}
