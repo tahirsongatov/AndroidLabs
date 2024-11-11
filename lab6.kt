5294ca93158f141b4b31d02665f79f47a5c8b3a5
Tagir Songatov <songatov@gmail.com>
2024-11-11 23:31:27 +0300

    High-order function sortBy

--- lab6.kt
+++ lab6.kt
@@ -594,3 +594,67 @@ fun main() {
 	println("Total price: $${totalPrice}")
 
 }
+// sort by
+class Cookie(
+    val name: String,
+    val softBaked: Boolean,
+    val hasFilling: Boolean,
+    val price: Double
+)
+val cookies = listOf(
+    Cookie(
+        name = "Chocolate Chip",
+        softBaked = false,
+        hasFilling = false,
+        price = 1.69
+    ),
+    Cookie(
+        name = "Banana Walnut", 
+        softBaked = true, 
+        hasFilling = false, 
+        price = 1.49
+    ),
+    Cookie(
+        name = "Vanilla Creme",
+        softBaked = false,
+        hasFilling = true,
+        price = 1.59
+    ),
+    Cookie(
+        name = "Chocolate Peanut Butter",
+        softBaked = false,
+        hasFilling = true,
+        price = 1.49
+    ),
+    Cookie(
+        name = "Snickerdoodle",
+        softBaked = true,
+        hasFilling = false,
+        price = 1.39
+    ),
+    Cookie(
+        name = "Blueberry Tart",
+        softBaked = true,
+        hasFilling = true,
+        price = 1.79
+    ),
+    Cookie(
+        name = "Sugar and Sprinkles",
+        softBaked = false,
+        hasFilling = false,
+        price = 1.39
+    )
+)
+
+fun main() {
+    val alphabeticalMenu = cookies.sortedBy {
+        it.name
+    }
+	println("Alphabetical menu:")
+    alphabeticalMenu.forEach {
+        println(it.name)
+    }
+
+}
+
+
