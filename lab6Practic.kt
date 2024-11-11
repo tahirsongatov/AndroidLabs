350a91f5322baf56435456fb170d16cc89eb57fe
Tagir Songatov <songatov@gmail.com>
2024-11-11 23:50:13 +0300

    Practice task 2

--- lab6Practic.kt
+++ lab6Practic.kt
@@ -17,3 +17,22 @@ fun main(){
     )
     println(event)
 }
+// Task 2
+data class Event (
+    val title: String,
+    val description: String? = null,
+    val daypart: Daypart,
+    val duration: Int,
+)
+enum class Daypart {
+    Morning, Afternoon,Evening
+}
+fun main(){
+    val event = Event(
+    	title = "Study Kotlin",
+        description = "Commit to studying Kotlin at least 15 minutes per day.",
+        daypart = Daypart.Evening,
+        duration = 15,
+    )
+    println(event)
+}
