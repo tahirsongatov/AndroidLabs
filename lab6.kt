80dce2e78a0d6c9d6c1fb2b5d1db7ed04f6cd257
Tagir Songatov <songatov@gmail.com>
2024-11-11 21:56:55 +0300

    Scope f\unctions

--- lab6.kt
+++ lab6.kt
@@ -147,4 +147,69 @@ fun main() {
     println(question1.toString())
 }
 
+// scope functions
+data class Question<T>(
+    val questionText: String,
+    val answer: T,
+    val difficulty: Difficulty,
+)
+enum class Difficulty {
+    EASY, MEDIUM, HARD
+}
+interface ProgressPrintable {
+    val progressText: String
+    fun printProgressBar()
+}
+
+class Quiz: ProgressPrintable {
+    val question1 = Question<String>("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
+    val question2 = Question<Boolean>("The sky is green. True or false", false, Difficulty.EASY)
+    val question3 = Question<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
+	override val progressText: String 
+    	get() = "${answered} of ${total} answered"
+
+    companion object StudentProgress {
+        var total: Int = 10
+        var answered: Int = 3
+    }
+    override fun printProgressBar() {
+        repeat(Quiz.answered) { print("▓") }
+        repeat(Quiz.total - Quiz.answered) { print("▒") }
+        println(progressText)
+	}
+    fun printQuiz() {
+    question1.let {
+        println(it.questionText)
+        println(it.answer)
+        println(it.difficulty)
+    }
+    println()
+    question2.let {
+        println(it.questionText)
+        println(it.answer)
+        println(it.difficulty)
+    }
+    println()
+    question3.let {
+        println(it.questionText)
+        println(it.answer)
+        println(it.difficulty)
+    }
+    println()
+}
+}
+fun main() {
+    val question1 = Question<String>("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
+    val question2 = Question<Boolean>("The sky is green. True or false", false, Difficulty.EASY)
+    val question3 = Question<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
+    println("${Quiz.answered} of ${Quiz.total} answered.")
+    Quiz().printProgressBar()
+    println(question1.toString())
+    val quiz = Quiz()
+    quiz.printQuiz()
+    Quiz().apply {
+        printQuiz()
+    }
+}
+
 
