adbda154fc41222a9f65dc1b73392744fd51fc18
Tagir Songatov <songatov@gmail.com>
2024-11-11 20:56:35 +0300

    Use an enum class

--- lab6.kt
+++ lab6.kt
@@ -8,4 +8,18 @@ fun main() {
     val question2 = Question<Boolean>("The sky is green. True or false", false, "easy")
     val question3 = Question<Int>("How many days are there between full moons?", 28, "hard")
 }
+// enums
+class Question<T>(
+    val questionText: String,
+    val answer: T,
+    val difficulty: Difficulty,
+)
+enum class Difficulty {
+    EASY, MEDIUM, HARD
+}
+fun main() {
+    val question1 = Question<String>("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
+    val question2 = Question<Boolean>("The sky is green. True or false", false, Difficulty.EASY)
+    val question3 = Question<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
+}
 
