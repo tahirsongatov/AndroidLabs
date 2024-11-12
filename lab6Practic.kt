e087c92f8035580302487f429a8be5292fb6b2ab
Tagir Songatov <songatov@gmail.com>
2024-11-12 11:42:20 +0300

    Practice Task 6 and 7

--- lab6Practic.kt
+++ lab6Practic.kt
@@ -106,6 +106,57 @@ fun main(){
         it.daypart
     }
     daypartEvents.forEach { (daypart,events)->
-        println("${itdaypart}: ${events.size} events.")
+        println("${daypart}: ${events.size} events.")
     }
+}
+// Task 6
+data class Event (
+    val title: String,
+    val description: String? = null,
+    val daypart: Daypart,
+    val durationInMinutes: Int,
+)
+enum class Daypart {
+    Morning, Afternoon,Evening
+}
+fun main(){
+    val events = mutableListOf(
+    	Event(title = "Wake up", description = "Time to get up", daypart = Daypart.Morning, durationInMinutes = 0),
+       	Event(title = "Eat breakfast", daypart = Daypart.Morning, durationInMinutes = 15),
+        Event(title = "Learn about Kotlin", daypart = Daypart.Afternoon, durationInMinutes = 30),
+        Event(title = "Practice Compose", daypart = Daypart.Afternoon, durationInMinutes = 60),
+        Event(title = "Watch latest DevBytes video", daypart = Daypart.Afternoon, durationInMinutes = 10),
+        Event(title = "Check out latest Android Jetpack library", daypart = Daypart.Evening, durationInMinutes = 45),
+    )
+    println("Last event of day: ${events.last().title}")
+}
+// Task 7
+data class Event (
+    val title: String,
+    val description: String? = null,
+    val daypart: Daypart,
+    val durationInMinutes: Int,
+)
+enum class Daypart {
+    Morning, Afternoon,Evening
+}
+val Event.durationOfEvent: String 
+    get() {
+        if (this.durationInMinutes < 60) {
+            return "short"
+        } else {
+            return "long"
+        }
+    }
+
+fun main(){
+    val events = mutableListOf(
+    	Event(title = "Wake up", description = "Time to get up", daypart = Daypart.Morning, durationInMinutes = 0),
+       	Event(title = "Eat breakfast", daypart = Daypart.Morning, durationInMinutes = 15),
+        Event(title = "Learn about Kotlin", daypart = Daypart.Afternoon, durationInMinutes = 30),
+        Event(title = "Practice Compose", daypart = Daypart.Afternoon, durationInMinutes = 60),
+        Event(title = "Watch latest DevBytes video", daypart = Daypart.Afternoon, durationInMinutes = 10),
+        Event(title = "Check out latest Android Jetpack library", daypart = Daypart.Evening, durationInMinutes = 45),
+    )
+    println("Duration of first event of the day: ${events[0].durationOfEvent}")
 }
