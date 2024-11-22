72ba5e7be42fba9a1747f6d02b9e6732154c28af
Tagir Songatov <songatov@gmail.com>
2024-11-22 16:32:51 +0300

    Content AffirmationCard

--- Affirmation/app/src/main/java/com/example/affirmation/MainActivity.kt
+++ Affirmation/app/src/main/java/com/example/affirmation/MainActivity.kt
@@ -4,15 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.affirmation.model.Affirmation
 import com.example.affirmation.ui.theme.AffirmationTheme
 
@@ -34,8 +43,24 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun AffirmationCard(affirmation: Affirmation) {
-
+fun AffirmationCard(affirmation: Affirmation, modifier: Modifier = Modifier) {
+    Card(modifier = modifier) {
+        Column {
+            Image(
+                painter = painterResource(id = affirmation.imageResourceId),
+                contentDescription = stringResource(id = affirmation.stringResourceId),
+                modifier = Modifier
+                    .fillMaxSize()
+                    .height(194.dp),
+                contentScale = ContentScale.Crop,
+            )
+            Text(
+                text = LocalContext.current.getString(affirmation.stringResourceId),
+                modifier = Modifier.padding(16.dp),
+                style = MaterialTheme.typography.headlineSmall,
+            )
+        }
+    }
 }
 
 @Composable
