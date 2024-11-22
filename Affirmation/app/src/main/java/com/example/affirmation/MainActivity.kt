4e31e14e4a0d90a718a3a0473bc561b028fe05b3
Tagir Songatov <songatov@gmail.com>
2024-11-22 16:23:18 +0300

    Create AffirmationCard composable

--- Affirmation/app/src/main/java/com/example/affirmation/MainActivity.kt
+++ Affirmation/app/src/main/java/com/example/affirmation/MainActivity.kt
@@ -6,11 +6,14 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.affirmation.model.Affirmation
 import com.example.affirmation.ui.theme.AffirmationTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +22,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AffirmationTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background,
+                ) {
+                    AffirmationApp()
                 }
             }
         }
@@ -31,17 +34,11 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun AffirmationCard(affirmation: Affirmation) {
+
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    AffirmationTheme {
-        Greeting("Android")
-    }
+fun AffirmationApp() {
+
 }
